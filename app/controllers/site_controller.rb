d00a0913fec9ff43902bd6fb21c92a2fd1924f19
jrsahuquillo <jrsahuquillo@gmail.com>
2016-11-14 11:13:25 +0100

    Add calculator page and calculate method

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -6,4 +6,16 @@ class SiteController < ApplicationController
   def contact
     render 'contact'
   end
+
+  def calculator
+    render 'calculator'
+  end
+
+  def calculate
+    @num1 = params[:first_num].to_f
+    @num2 = params[:second_num].to_f
+    @result = @num1 + @num2
+    render 'calculate'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   get'/contact', to: 'site#contact'
 
+  get'/calculator', to: 'site#calculator'
+
+  post '/calculate', to: 'site#calculate'
+
 end
