8ed53b428f70b3213ee03294e295c596d3a99b4e
jrsahuquillo <jrsahuquillo@gmail.com>
2016-11-16 12:01:24 +0100

    Time entries seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Project.create!(name: 'Ironhack', description: 'Ironhack is a...')
-Project.create!(name: 'Time tracking app')
-Project.create!(name: 'Recipes', description: 'Track my favorite recipes')
-Project.create!(name: 'Exercises', description: 'Track my last exercises')
-Project.create!(name: 'Games')
-Project.create!(name: 'Programs', description: 'Code projects')
+# Project.create!(name: 'Ironhack', description: 'Ironhack is a...')
+# Project.create!(name: 'Time tracking app')
+# Project.create!(name: 'Recipes', description: 'Track my favorite recipes')
+# Project.create!(name: 'Exercises', description: 'Track my last exercises')
+# Project.create!(name: 'Games')
+# Project.create!(name: 'Programs', description: 'Code projects')
 
-7.upto(14).each do |i|
+1.upto(10).each do |i|
   Project.create!(name: "Ironhack#{i}", description: "description #{i}")
+  project.time_entries.create(minutes: rand(1..60), hours: rand(0..2))
 end
