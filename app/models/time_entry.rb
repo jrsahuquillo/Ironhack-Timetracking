175184b8b358b9d2b6cb4965ce59350459b2fc71
jrsahuquillo <jrsahuquillo@gmail.com>
2016-11-16 17:27:34 +0100

    Finished Exercise

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -2,5 +2,5 @@ class TimeEntry < ApplicationRecord
   belongs_to :project
   validates :hours, presence: true, numericality: true
   validates :minutes, presence: true, numericality: true
-  validates :date, presence: true,
+  validates :date, presence: true
 end
