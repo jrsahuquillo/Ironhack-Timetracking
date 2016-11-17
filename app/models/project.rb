7d5601b98dc5afd2ce24cf4b2194d5912e9e81e6
jrsahuquillo <jrsahuquillo@gmail.com>
2016-11-17 11:02:17 +0100

    Edit time entries an delete old projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,9 @@ class Project < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :name, length: {maximum: 30}
   validates :name, format: {with: /(\w\s)+/}
+
+  def self.clean_old(date = 1.week.ago)
+    where( 'created_at < ?', date ).destroy_all
+  end
+
 end
